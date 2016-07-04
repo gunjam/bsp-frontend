2884378a906266a9ed46d13a03b4a9478a311953
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-07-04 17:15:42 +0100

    Add start page with i18next language support.

--- server.js
+++ server.js
@@ -2,19 +2,27 @@ require('marko/node-require').install();
 
 const express = require('express');
 const compression = require('compression');
+const i18next = require('i18next');
+const FilesystemBackend = require('i18next-node-fs-backend');
+const i18nextMiddleware = require('i18next-express-middleware');
 
 // Configure Lasso.js
 require('lasso').configure(require('./config/lasso'));
 
+// Setup i18next
+i18next
+  .use(FilesystemBackend)
+  .use(i18nextMiddleware.LanguageDetector)
+  .init(require('./config/i18next'));
+
 const app = express();
 const port = 3000;
 
+app.use(i18nextMiddleware.handle(i18next));
 app.use(compression());
 app.use(require('lasso/middleware').serveStatic());
 
-app.get('/', require('./src/pages/index'));
-app.get('/form', require('./src/pages/form'));
-app.get('/radios', require('./src/pages/radios'));
+app.get('/start', require('./src/pages/start'));
 
 app.listen(port, err => {
   if (err) {
