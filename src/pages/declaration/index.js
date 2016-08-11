146a8d1def4de8546842e308e1ec40c2b0ab2218
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-08-11 15:06:27 +0100

    Prevent error on declaration GET if payment type wasnt in session

--- src/pages/declaration/index.js
+++ src/pages/declaration/index.js
@@ -1,7 +1,7 @@
 const template = require('./template.marko');
 
 module.exports = function (req, res) {
-  const backLink = req.session.payment.type === 'bank' ? '/bank-details' :
-    '/building-society-details';
+  const backLink = (req.session.payment || {}).type === 'bank' ?
+    '/bank-details' : '/building-society-details';
   template.render({backLink}, res);
 };
