d8ffab1547bb5268a5604cca72f3b2355d645c6e
Niall Molloy <niallmolloy.digitaldwp@gmail.com>
2016-08-19 13:22:37 +0100

    Update assertion fail messages to be relevant

--- src/test/is-valid-date-object.js
+++ src/test/is-valid-date-object.js
@@ -4,27 +4,27 @@ const {assert} = require('chai');
 const isValidDateObject = require('../utils/is-valid-date-object');
 
 describe('isValidDateObject()', () => {
-  it(`should return true if input properties form a valid date`, () => {
+  it('should return true if input properties form a valid date', () => {
     const dateObject = {day: '19', month: '08', year: '2016'};
     const output = isValidDateObject(dateObject);
-    assert.isTrue(output, 'wasn\'t true when input was a string of numbers');
+    assert.isTrue(output, 'wasn\'t true when input date was valid');
   });
 
-  it(`should return false if input properties form an invalid date`, () => {
+  it('should return false if input properties form an invalid date', () => {
     const dateObject = {day: '31', month: '02', year: '2016'};
     const output = isValidDateObject(dateObject);
-    assert.isFalse(output, 'wasn\'t true when input was a string of numbers');
+    assert.isFalse(output, 'wasn\'t false when input date was invalid');
   });
 
-  it(`should return false if any input properties aren't ints`, () => {
+  it('should return false if any input properties don\'t parse as ints', () => {
     const dateObject = {day: '31', month: 'A', year: '2016'};
     const output = isValidDateObject(dateObject);
-    assert.isFalse(output, 'wasn\'t true when input was a string of numbers');
+    assert.isFalse(output, 'wasn\'t false when property doesn\'t parse as int');
   });
 
-  it(`should return false if input year property isn't 4 digits`, () => {
+  it('should return false if input year property isn\'t 4 digits', () => {
     const dateObject = {day: '19', month: '01', year: '96'};
     const output = isValidDateObject(dateObject);
-    assert.isFalse(output, 'wasn\'t true when input was a string of numbers');
+    assert.isFalse(output, 'wasn\'t false when input year was not 4 digits');
   });
 });
